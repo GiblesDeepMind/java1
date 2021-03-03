dac0d2b34b53440ecb9fadbf883cc19063b5c67c
giblesdeepmind <ehdals1323@gmail.com>
2021-03-03 21:34:21 +0900

    first.java

---
+++ Data_and_operation/StringOperation.java
@@ -0,0 +1,12 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World".length());
+		System.out.println("Hello, leezche ... bye".replace("leezche", "egoing"));
+		
+	}
+
+}
